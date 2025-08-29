183dd1072cbf3ff6338f878b1853a845adb845eb
petchtany <petchtany123@gmail.com>
2025-08-29 17:43:41 +0700

    commit1

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// app
+const app = express();
+
+
+// connect db
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true
+    
+})
+    .then(()=>console.log('DB Connected'))
+    .catch((err)=>console.log('Cannot Connect to DB',err))
+
+
+
+
+//middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json({limit: "2mb"}));
+app.use(cors());
+
+
+
+//route
+app.get('/api',(req,res) =>{
+    res.send('hello World')
+})
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=> console.log('Server is Running On port ',port));
